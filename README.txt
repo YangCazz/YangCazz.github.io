ef6e1826b79003a2334be5707828ee0ef2bb45ca
小杨同学 <54882633+YangCazz@users.noreply.github.com>
2023-05-26 14:40:40 +0800

    change test
    修改网页模板

--- README.txt
+++ README.txt
@@ -1,17 +1,11 @@
-Striped by HTML5 UP
+Halcyonic by HTML5 UP
 html5up.net | @ajlkn
 Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 
 
-And here's a new one: Striped, which features a clean, minimalistic design, styling for
-all basic page elements (including blockquotes, tables and lists), a repositionable
-sidebar (left or right), and HTML5/CSS3 code designed for quick and easy customization
-(see code comments for details).
-
-Demo images* courtesy of Unsplash, a radtastic collection of CC0 (public domain) images
-you can use for pretty much whatever.
-
-(* = Not included)
+A somewhat unusual but interesting responsive site template. I did this one to test out a
+new pattern set I put together and I think it really came together well. As for use cases
+it's kind of unspecific so hey, use it for whatever.
 
 Feedback, bug reports, and comments are not only welcome, but strongly encouraged :)
 
@@ -24,12 +18,6 @@ Credits:
 	Demo Images:
 		Unsplash (unsplash.com)
 
-	Icons:
-		Font Awesome (fortawesome.github.com/Font-Awesome)
-
 	Other:
 		jQuery (jquery.com)
-		html5shiv.js (@afarkas @jdalton @jon_neal @rem)
-		CSS3 Pie (css3pie.com)
-		Respond.js (j.mp/respondjs)
-		Skel (skel.io)
+		Responsive Tools (github.com/ajlkn/responsive-tools)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,42 @@
 /*
-	Striped by HTML5 UP
+	Halcyonic by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$document = $(document);
+	var $window = $(window),
+		$body = $('body');
 
 	// Breakpoints.
 		breakpoints({
-			desktop:   [ '737px',   null     ],
-			wide:      [ '1201px',  null     ],
-			narrow:    [ '737px',   '1200px' ],
-			narrower:  [ '737px',   '1000px' ],
-			mobile:    [ null,      '736px'  ]
-		});
-
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
 		});
 
 	// Nav.
 
-		// Height hack.
-		/*
-			var $sc = $('#sidebar, #content'), tid;
-
-			$window
-				.on('resize', function() {
-					window.clearTimeout(tid);
-					tid = window.setTimeout(function() {
-						$sc.css('min-height', $document.height());
-					}, 100);
-				})
-				.on('load', function() {
-					$window.trigger('resize');
-				})
-				.trigger('resize');
-		*/
-
 		// Title Bar.
 			$(
 				'<div id="titleBar">' +
-					'<a href="#sidebar" class="toggle"></a>' +
+					'<a href="#navPanel" class="toggle"></a>' +
 					'<span class="title">' + $('#logo').html() + '</span>' +
 				'</div>'
 			)
 				.appendTo($body);
 
-		// Sidebar
-			$('#sidebar')
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
 				.panel({
 					delay: 500,
 					hideOnClick: true,
@@ -64,7 +45,7 @@
 					resetForms: true,
 					side: 'left',
 					target: $body,
-					visibleClass: 'sidebar-visible'
+					visibleClass: 'navPanel-visible'
 				});
 
 })(jQuery);
